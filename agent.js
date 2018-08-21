d58dd42520411d9c9c57cfc96b00ba84463a7108
高橋　淳二 <>
2018-08-21 14:06:53 +0900

    update agent.js

--- agent.js
+++ agent.js
@@ -46,40 +46,40 @@ setInterval(function () {
 
 noble.on('discover', function (peripheral) {
   if (peripheral.advertisement && peripheral.advertisement.manufacturerData) {
-    var data = peripheral.advertisement.manufacturerData.toString("hex");
-    var buffer = peripheral.advertisement.manufacturerData;
-    var mac = peripheral.id;
-    mac = mac.match(/[0-9a-z]{2}/g).join(":");
+    var manufacturerData = peripheral.advertisement.manufacturerData;
+    var type = manufacturerData.toString("hex");
+    var buffer = manufacturerData;
+    var uuid = peripheral.id;
+    var macAddress = peripheral.id.match(/[0-9a-z]{2}/g).join(":");
+    var rssi = peripheral.rssi;
     var now = new Date();
-    //console.log(mac + "," + rssi + "," + data + ",-," + now.toISOString());
-
+    
     // Company ID = OMRON Env センサー
     // SCAN_RSP PDU Payload (37 octets)
     // AD 2 Company ID 5d02
-    if (data.startsWith("d502")) {
+    if (type.startsWith("d502")) {
       if (buffer.length < 22) {
         console.log(mac + " is not configure OMRON-Env. Expected AD 2 length 22, actual " + buffer.length);
       } else {
         // console.log("OMRON-Env," + mac + "," + rssi + "," + data + ",-," + now.toISOString());
         var envData;
-        var isApTarget;
         try {
           var dataOffset = -5;
           envData = {
-            APID: 'sample'/*config.apId*/,
-            ID: mac,
-            isApTarget: isApTarget,
-            Temperature: buffer.readInt16LE(dataOffset + 8) / 100,  // 単位：0.01 degC
-            Humidity: buffer.readUInt16LE(dataOffset + 10) / 100,   // 単位：0.01 %RH
-            ambient_light: buffer.readUInt16LE(dataOffset + 12),    // 単位：1 lx
-            uv_index: buffer.readUInt16LE(dataOffset + 14) / 100,   // 単位：0.01
-            pressure: buffer.readUInt16LE(dataOffset + 16) / 10,    // 単位：0.1 hPa
-            Noise: buffer.readUInt16LE(dataOffset + 18) / 100,      // 単位：0.01 dB
-            acceleration_x: buffer.readUInt16LE(dataOffset + 20),
-            acceleration_y: buffer.readUInt16LE(dataOffset + 22),
-            acceleration_z: buffer.readUInt16LE(dataOffset + 24),
-            battery_voltage: (buffer.readUInt8(dataOffset + 26) + 100) * 10, // ((取得値 + 100) x 10) mV
-            timastamp: now.toISOString()
+              UUID: uuid,
+              ID: macAddress,
+              rssi: rssi,
+              Temperature: buffer.readInt16LE(dataOffset + 8) / 100,  // 単位：0.01 degC
+              Humidity: buffer.readUInt16LE(dataOffset + 10) / 100,   // 単位：0.01 %RH
+              ambient_light: buffer.readUInt16LE(dataOffset + 12),    // 単位：1 lx
+              uv_index: buffer.readUInt16LE(dataOffset + 14) / 100,   // 単位：0.01
+              pressure: buffer.readUInt16LE(dataOffset + 16) / 10,    // 単位：0.1 hPa
+              Noise: buffer.readUInt16LE(dataOffset + 18) / 100,      // 単位：0.01 dB
+              acceleration_x: buffer.readUInt16LE(dataOffset + 20),
+              acceleration_y: buffer.readUInt16LE(dataOffset + 22),
+              acceleration_z: buffer.readUInt16LE(dataOffset + 24),
+              battery_voltage: (buffer.readUInt8(dataOffset + 26) + 100) * 10, // ((取得値 + 100) x 10) mV
+              timastamp: now.toISOString()
           };
           isApTarget = mac == config.envMac;
         } catch (err) {
